4ec1a3082fe939ca1f3ad9afefd1704b7c775678
kjh5848 <kjh5848@gmail.com>
2025-11-27 23:20:56 +0900

    폴링

--- src/main/java/com/metacding/polling/chat/ChatRepository.java
+++ src/main/java/com/metacding/polling/chat/ChatRepository.java
@@ -2,6 +2,5 @@ package com.metacding.polling.chat;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// (3-2)/(4-2) 서비스에서 폴링 조회·저장 시 사용하는 JPA 리포지토리
 public interface ChatRepository extends JpaRepository<Chat, Integer> {
 }
--- src/main/java/com/metacding/polling/chat/ChatRequest.java
+++ src/main/java/com/metacding/polling/chat/ChatRequest.java
@@ -1,5 +1,4 @@
 package com.metacding.polling.chat;
 
-// (4-0) POST /chats 요청 바디를 전달하는 DTO
 public record ChatRequest(String message) {
 }
